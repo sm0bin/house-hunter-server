ec48ea6af109f1b57af9a3ac2efd8cf8d77cae12
GhostMobin <s1810677110@ru.ac.bd>
2024-01-23 23:25:49 +0600

    users created

---
+++ routeHandler/userHandler.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const User = require('../schemas/usersSchema');
+const jwt = require("jsonwebtoken");
+
+router.post('/signup', async (req, res) => {
+    const { fullName, role, phoneNumber, email, password } = req.body;
+    try {
+        const user = await User.create({ fullName, role, phoneNumber, email, password });
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+        res.send({ token });
+    } catch (err) {
+        console.log(err);
+        res.status(422).send(err);
+    }
+})
+
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password)
+        return res.status(422).send({ message: "Email and password are required!" });
+    try {
+        const user = await User.findOne({ email });
+        if (!user)
+            return res.status(422).send({ message: "Invalid email or password!" });
+        if (user.password !== password)
+            return res.status(422).send({ message: "Invalid email or password!" });
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+        res.send({ token });
+    } catch (err) {
+        console.log(err);
+        res.status(422).send({ message: "Invalid email or password!" });
+    }
+})
+
+module.exports = router;
---
+++ shemas/usersSchema.js
@@ -0,0 +1,32 @@
+// full name, role (House Owner or
+//     House Renter) (it must be selected as an option), phone number, email, and password
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: [true, "Full name is required!"]
+    },
+    role: {
+        type: String,
+        required: [true, "Role is required!"]
+    },
+    phoneNumber: {
+        type: String,
+        required: [true, "Phone number is required!"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required!"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required!"]
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
